2bcb702479dd5b66fc27468958af647f60b132c0
Jake Richmond <jakeorichmond@gmail.com>
2015-10-10 11:12:28 -0700

    JS

--- myfirstpage.js
+++ myfirstpage.js
@@ -1,3 +1,27 @@
 <script>
 	alert("Hello World.");
 </script>
+
+
+
+
+// A WHILE loop repeats a block of code WHILE a condition is true.
+// Like and IF statement, the condition is found in the parentheses.
+var i = 1;
+while (i<10) {
+	alert(i);
+	i = i+1;
+}
+
+// A FOR loop is similar to an IF statement, but combines three-
+// semicolon-seperated pieces of information between the parentheses:
+// Initializaton, condition, and a final expression.
+// The initialization part is for creating a variable to let you track
+// how far through the loop you are - like i in the WHILE example;
+// the condition is where the looping logic goes - the same as the condition
+// in the while example; and the final expression is run at 
+// the end of every loop. 
+
+for (var i = 1; i < 10; i++){
+	alert(i);
+}
