8463e54d08f7ea94c0a59a1343e8595a6d6327e7
Jake Richmond <jakeorichmond@gmail.com>
2015-10-22 12:44:00 -0700

    JS

--- myfirstpage.js
+++ myfirstpage.js
@@ -168,3 +168,115 @@ for (var i=0; i < people.length; i++) {
 // The Document Object Model is a way to manipulate the structure and style of an HTML page.
 // it represents the internals of the page as the browser sees it, and allows the developer
 // to alter it with JavaScript.
+
+// Trees and Branches
+// HTML is an XML-like structure in that the elements form a structure of parents' nodes
+// with children, like the branches of a tree. There is one root element (html) with
+// branches like head and body, each with their own branches. For this reason, the DOM is
+// also called the DOM tree.
+
+// Modifying the DOM, by picking an element and changing something about it, is something
+// done often in JavaScript, the document object is used. It's provided by the browser and
+// allows code on the page to interact with the content.
+
+// Getting an Element
+
+// The firtst thing to know is how to get an element. There are a number of ways of doing
+// it, and browsers support different ones. Starting with the best supported we'll move
+// through to the latest, and most useful, versions.
+
+// By ID
+// document.getElementById is a method for getting hold of an element - unsurprisingly - 
+// by its ID.
+
+var pageHeader = document.getElementById('page-header');
+
+// The pageHeader element can then be manipulated - its size and color can be changed,
+// and other code can be declared to handle the element being clicked on or hovered over.
+// It's supported in pretty much all the browsers you need to worry about.
+
+// Notice that getElementById is a method of the document object. Many of the methods used
+// to access the page are found on the document object.
+
+// By Tag Name
+// document.getElementsByTagName works in the same way as getElementById, except that it
+// takes a tag name (a, ul, li, etc) instead of an ID and returns a NodeList, which is
+// essentially an array of the DOM Elements.
+
+// By Class Name
+// document.getElementsByClassName returns the same kind of NodeList as
+// getElementsByTagName, except that you pass a class name to be matched, not a tag name.
+
+// By CSS Selector
+// A couple of new methods are available in modern browsers that make selecting elements
+// easier by allowing the use of CSS selectors. They are document.querySelector and
+// document.querySelectorAll
+
+var pageHeader = document.querySelector('#header');
+var buttons = document.querySelectorAll(.btn);
+
+// querySelector, like getElementById, returns only one element whereas querySelectorAll
+// returns a NodeList. If multiple elements match the selector you pass to querySelector,
+// only the first will be returned.
+
+// Events and Callbacks
+
+// In the browser most code is event-driven and writing interactive applications in 
+// JavaScript is often about waiting for and reacting to events, to alter the beavior
+// of the browser in some way. Events occur when the page loads, when a user interacts,
+// (clicks, hovers, changes) and a myraid of other times, can be triggered manually too.
+
+// To react to an event you listen for it and supply a function which will be called by 
+// the browser when the event occurs. This function is known as a callback.
+
+// Here's a group of the thigns needed to listen for an event; the callback function,
+// an element and the call to listen for an event:
+
+var handleClick = function (event) {
+	// Do something!
+};
+
+var button = document.querySelector('#big-button');
+button.addEventListener('click', handleClick);
+
+// addEventListener is a method found on all DOM elements. Here it's being called on an
+// element saved in the button variable. The first argument is a string - the name of the
+// event to listen for. Here's it's click - that's a click of the mouse or a tap of the 
+// finger. The second is the callback function - here it's handleClick.
+
+// Data about a particular event is passed to the event callback. Take a look at 
+// handleClick, declared above. You can see its argument: event - it's an object whose
+// properties describe what occured.
+
+// Here's an example event you might see into a click event callback like handleClick.
+// There are lots of properties giving you an idea of where the event occured on the page
+// (like pageX and offsetY) - these are slightly different because they depend on the 
+// reference point used to measure from. You can also see the target which is a reference 
+// to the node that was clicked.
+
+{
+	offsetX: 74,
+	offsetY: 10,
+	pageX: 154,
+	pageY: 576,
+	screenX: 154,
+	screenY: 489,
+	target: h2,
+	timeStamp: 1363131952985
+	type: "click",
+	x: 154,
+	y: 395
+}
+
+// AJAX
+
+// In the early years of the web, things were simple - a page was text, perhaps with
+// styling, and it contained links to other pages. To get new content you moved from
+// one page to the next. But as developers got more ambitious with the web, attempting
+// to build interactive ("native-like" applications), it was obvious that there 
+// needed to be a way to load new content into a page without a full reload.
+
+// To retrieve new content for a page, like new articles on an infinite-scroll site or
+// to notify you of new emails, a tool called XML HTTP REQUEST (XHR) is used. Web apps
+// that do this are also callled AJAX apps, AJAX standing for Asynchronous JavaScript and
+// XML.
